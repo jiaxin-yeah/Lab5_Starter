2e7b214cc14b3e9431f9b08e776cb2602f9e7efe
jiaxin-yeah <jiaxinbehappy@gmail.com>
2022-10-26 15:15:29 -0700

    sth

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,24 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+function imgAudio(horn) {
+  const img = document.querySelector('img[src="assets/images/no-image.png"]');
+  const audio = document.getElementsByClassName("hidden");
+  if (horn.target.value == "air-horn") {
+    img.src = "assets/images/air-horn.svg";
+    audio.src = "assets/audio/air-horn.mp3";
+  }
+  else if (horn.target.value == "car-horn") {
+    img.src = "assets/images/car-horn.svg";
+    audio.src = "assets/audio/car-horn.mp3";
+  }
+  else if (horn.target.value == "party-horn") {
+    img.src = "assets/images/party-horn.svg";
+    audio.src = "assets/audio/party-horn.mp3";
+  }
+}
+
 function init() {
   // TODO
+  const horn = document.getElementsByClassName("horn");
 }
